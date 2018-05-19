4bd393cc7de47a5158ad2a5799cfa452bbb65d03
Yuriy Tymkiv <11hacha11@gmail.com>
2018-05-19 21:01:33 +0300

    starter

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-let token;
+const TelegramBot = require("node-telegram-bot-api")
+
+const token = 'token_here'
+const bot = new TelegramBot(token, {polling: true})
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Hello")
+})
