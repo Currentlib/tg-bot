ab6305e0b589736f92996fdf192b16f7837c52ed
Yuriy Tymkiv <11hacha11@gmail.com>
2018-05-19 22:53:12 +0300

    instruction

--- index.js
+++ index.js
@@ -34,10 +34,27 @@ function replyKeyBoard(param) {
 }
 
 function grandMenu() {
-    bot.on("message", (msg)=>{
+    bot.on("message", (msg)=>{ /*5. таксамо лісенер
+        просто інколи треба буде (можливо) забрати ліснер message, 
+        в тому тітто часто мені то треба було, бо там бот реагвав на фото
+        і відповідно там був код щось тиу
+                bot.on("text", (txt)=>{
+                    bot.removeListener("message");
+                    ЩОСЬ ТУТ РОБИТЬСЯ
+                    bot.removeListener("text");
+                    grandMenu();
+                })
+        бо і той і той лісенер мали певні реакції на текст і шоб не було конфліктів
+
+        */
         switch (msg.text) {
             case 'Наявність товару/Купити': 
-                bot.sendMessage(msg.chat.id, "Меню якесь буде")
+                bot.sendMessage(msg.chat.id, "Введіть кількість чогось")
+                bot.on("text", (txt)=>{ //1. команда шо запускає лісенер, один раз запустив і він вічно провіряє
+                    //2. якийсь код шо кудись там зберігає то шо користувач ввів через txt.data
+                    bot.sendMessage(msg.chat.id, "Все вірно додано") //3. відправляєм користувачу підтвердження
+                    bot.removeListener("text"); //4. і забираємо лісенер, бо він буде реагувати на всі повідомлення текстові і надалі
+                })
                 break;
             case 'Підтримка/Питання': 
                 bot.sendMessage(msg.chat.id, "Лише в про версії")
