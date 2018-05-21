fba4098dbac80f8e8a82d2b717993dcfcef6261b
Dimitri Ishchuk <ischuk.d.99@gmail.com>
2018-05-21 14:40:03 +0300

    cleanup

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const config = require("./config.json")
 const bot = new TelegramBot(config.token, {polling: true})
 var modercode = require('coupon-code');
 grandMenu();
-<<<<<<< HEAD
-=======
-//ssS
-//my comment
->>>>>>> 52788b0e65b2f0ae5ea8a7b48f72f2f8c053f66c
 
 //Очікування уоманди /start
 bot.onText(/\/start/, (msg) => {
@@ -127,21 +122,15 @@ function keygen () {
 	function getDateTime(time) {
 
     var date = new Date(time);
-
     var hour = date.getHours();
     hour = (hour < 10 ? "0" : "") + hour;
-
     var min  = date.getMinutes();
     min = (min < 10 ? "0" : "") + min;
-
     var sec  = date.getSeconds();
     sec = (sec < 10 ? "0" : "") + sec;
-
     var year = date.getFullYear();
-
     var month = date.getMonth() + 1;
     month = (month < 10 ? "0" : "") + month;
-
     var day  = date.getDate();
     day = (day < 10 ? "0" : "") + day;
 
