f7e21a1647073cb58316044b5c2bc7534a7042fe
Yuriy Tymkiv <11hacha11@gmail.com>
2018-05-21 12:57:19 +0300

    grfssgrgr

---
+++ admin.js
@@ -0,0 +1,10 @@
+function admin() {
+    bot.onText(new RegExp('\/admin'), msg=>{
+        if (msg.chat.id == config.admin) {
+            bot.sendMessage(msg.chat.id, 'Ти адмін!', replyKeyBoard("admin"))
+        } else {
+            bot.sendMessage(msg.chat.id, 'Ти НЕ адмін!!!')
+        }
+    })
+}
+
