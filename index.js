9ce63c16523416a0d66c42381f773e41e438e696
Dimitri Ishchuk <ischuk.d.99@gmail.com>
2018-05-23 15:21:22 +0300

    one-level menu

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const TelegramBot = require("node-telegram-bot-api")
 const config = require("./config.json")
 const bot = new TelegramBot(config.token, {polling: true})
 var modercode = require('coupon-code');
-grandMenu();
 //DATABASE
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
@@ -16,27 +15,25 @@ bot.onText(/\/start/, (msg) => {
 })
 
 //Очікування уоманди /start
-bot.onText(/\/test/, (msg) => {
-    console.log(msg)
+bot.onText(/\/emp/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Вітаю у меню працівника!", replyKeyBoard("employee"))
 })
 
 //Очікування команди вхду в адмінку
 bot.onText(new RegExp('\/admin'), msg=>{
     if (msg.chat.id == config.admin) {
         bot.sendMessage(msg.chat.id, 'Вітаю у адмін панелі!', replyKeyBoard("admin"));
-        adminInit(msg.chat.id);
     } else {
         bot.sendMessage(msg.chat.id, 'Ти НЕ адмін!!!')
     }
 })
 
 
-
-
 //Набір менюшок "Клавіатур"
 function replyKeyBoard(param) {
     let keys;
     let keyboard;
+    //Меню покупця
     if (param == "start") {
         keys = JSON.stringify({
             keyboard: [
@@ -51,7 +48,25 @@ function replyKeyBoard(param) {
             resize_keyboard: true
         });
         keyboard = {reply_markup: JSON.parse(keys)};
+
+        bot.on("message", (msg)=>{ 
+            switch (msg.text) {
+                case 'Купити':
+                    bot.sendMessage(msg.chat.id, msg.chat.id)
+                    break;
+                case 'Питання': 
+                    bot.sendMessage(msg.chat.id, "Напишіть повідомленя адміну")
+                    break;
+                case 'Профіль користувача': 
+                    bot.sendMessage(msg.chat.id, "Лише в про версії")
+                    break;
+            }
+        });
+    
+
     }
+
+    //Адмін меню
     if (param == "admin") {
         keys = JSON.stringify({
             keyboard: [
@@ -68,58 +83,34 @@ function replyKeyBoard(param) {
             resize_keyboard: true
         });
         keyboard = {reply_markup: JSON.parse(keys)};
+
+        bot.on("text", (msg)=>{ 
+            if (msg.chat.id == config.admin) {
+                switch (msg.text) {
+                    case 'Товар':
+                        bot.sendMessage(msg.chat.id, "Список товарів")
+                        break;
+                    case 'Модератори': 
+                        bot.sendMessage(msg.chat.id, "Список модерів") 
+                        break;
+                    case 'Згенерувати ключ': 
+                        bot.sendMessage(msg.chat.id, keygen())
+                        break;
+                    case 'Вийти': 
+                        bot.removeListener("text")
+                        bot.sendMessage(msg.chat.id, "Успішно", replyKeyBoard("start"))
+                        break;
+                }
+            } else {
+                bot.sendMessage(msg.chat.id, 'Ти НЕ адмін!!!')
+            }
+        })
     }
+
+
     return keyboard;
 }
 
-//Функція запуску роботи адмін меню
-function adminMenu() {
-    bot.on("text", (msg)=>{ 
-        if (msg.chat.id == config.admin) {
-            switch (msg.text) {
-                case 'Товар':
-                    bot.sendMessage(msg.chat.id, "Список товарів")
-                    break;
-                case 'Модератори': 
-                    bot.sendMessage(msg.chat.id, "Список модерів") 
-                    break;
-                case 'Згенерувати ключ': 
-                    bot.sendMessage(msg.chat.id, keygen())
-                    break;
-                case 'Вийти': 
-                    bot.removeListener("text")
-                    bot.sendMessage(msg.chat.id, "Успішно", replyKeyBoard("start"))
-                    break;
-            }
-        } else {
-            bot.sendMessage(msg.chat.id, 'Ти НЕ адмін!!!')
-        }
-    })
-}
-
-//Функція запуску загального меню
-function grandMenu() {
-    bot.on("message", (msg)=>{ 
-        switch (msg.text) {
-            case 'Купити':
-                bot.sendMessage(msg.chat.id, msg.chat.id)
-                break;
-            case 'Питання': 
-                bot.sendMessage(msg.chat.id, "Напишіть повідомленя адміну")
-                break;
-            case 'Профіль користувача': 
-                bot.sendMessage(msg.chat.id, "Лише в про версії")
-                break;
-        }
-    })
-}
-
-//Функція ініціалізації контролера адміна
-function adminInit(id) {
-	bot.sendMessage(id, "Що робитимемо?");
-	adminMenu();
-
-}
 //Генерація одноразовового кода реєстрації модера
 function keygen () {
 	var authcode = modercode.generate({ parts: 4, partLen : 6 });
