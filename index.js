18ca80611834305d7f49b24e7acf181302e98a14
Yuriy Tymkiv <11hacha11@gmail.com>
2018-06-10 22:34:37 +0300

    ссс

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 //Паттерн бд
-db.defaults({items: []})
-.write()
+//db.defaults({items: [], cities: [], regions: []})
+//.write()
 
 //Очікування уоманди /start
 bot.onText(/\/start/, (msg) => {
@@ -21,14 +21,36 @@ bot.onText(/\/start/, (msg) => {
 
 //Очікування уоманди /start
 bot.onText(/\/test/, (msg) => {
-    let abc = "start"
-    console.log(menu.start)
+    let base = db.get("cities")
+    .value()
+    let ret = []
+    base.forEach((cur, i)=>{
+        let par = Object.keys(base[i])
+        ret.push(par[0])
+    })
 })
 
 bot.on("callback_query", query=>{
     let parsed = JSON.parse(query.data)
     //console.log(query)
-    if (parsed.isEdit) {
+
+    if (parsed.isDelete && parsed.isCity) {
+        bot.sendMessage(query.from.id, `Ви впевнені, що хочете видалити "${parsed.name}"?`)
+        bot.on("text", text=>{
+            if (text.text.toLowerCase() == "так") {
+                db.get("cities")
+                    .remove({name: parsed.name})
+                    .value()
+                    db.write();
+                bot.removeListener("text")
+                bot.sendMessage(query.from.id, `Місто "${parsed.name}" успішно видалено!`)
+            } else {
+                bot.removeListener("text")
+                bot.sendMessage(query.from.id, `Місто "${parsed.name}" НЕ видалено!`)
+            }
+        })
+    }
+    if (parsed.isEdit && parsed.isItem) {
         bot.sendMessage(query.from.id, "Введіть нову назву товару")
         let obj = {
             name: "",
@@ -44,8 +66,8 @@ bot.on("callback_query", query=>{
                     db.get("items")
                     .find({name: parsed.name})
                     .assign({name: obj.name, price: obj.price}) 
-                    .write();
-                    db.save();
+                    .value();
+                    db.write();
                     bot.sendMessage(price.chat.id, "Товар успішно змінено");
                     bot.removeListener("text")
                 })
@@ -53,10 +75,33 @@ bot.on("callback_query", query=>{
         })
     }
 
-    if (parsed.isDelete) {
-        bot.sendMessage(query.from.id, `Ви впевнені, що хочете видалити ${parsed.name}?`)
+    if (parsed.isDelete && parsed.isItem) {
+        bot.sendMessage(query.from.id, `Ви впевнені, що хочете видалити "${parsed.name}"?`)
+        bot.on("text", text=>{
+            if (text.text.toLowerCase() == "так") {
+                db.get("items")
+                    .remove({name: parsed.name})
+                    .value()
+                    db.write();
+                bot.removeListener("text")
+                bot.sendMessage(query.from.id, `Товар "${parsed.name}" успішно видалено!`)
+            } else {
+                bot.removeListener("text")
+                bot.sendMessage(query.from.id, `Товар "${parsed.name}" НЕ видалено!`)
+            }
+        })
     }
 })
+
+//Масив міст
+function citiesMassive() {
+    let base = db.get("cities").value()
+    let ret = []
+    base.forEach((cur, i)=>{
+        ret.push(cur.name)
+    })
+    return ret;
+}
 
 //Набір кнопок при повідомленнях
 
@@ -66,12 +111,25 @@ function inlineKeyBoard(param, name) {
     if (param == "item") {
         keys = {
             inline_keyboard: [
-                [{text: "Редагувати", callback_data: JSON.stringify({name: name, isEdit: true})}, {text: 'Видалити', callback_data: JSON.stringify({name: name, isDelete: true})}]
+                [{text: "Редагувати", callback_data: JSON.stringify({name: name, isEdit: true, isItem: true})}, {text: 'Видалити', callback_data: JSON.stringify({name: name, isDelete: true, isItem: true})}]
             ]
         }
         keyboard = {
             reply_markup: JSON.stringify(keys)
         }
+    }
+    if (param == "city") {
+        keys = {
+            inline_keyboard: [
+                [{text: 'Видалити', callback_data: JSON.stringify({name: name, isDelete: true, isCity: true})}]
+            ]
+        }
+        keyboard = {
+            reply_markup: JSON.stringify(keys)
+        }
+    }
+    if (param == "regions") {
+
     }
     return keyboard;
 }
@@ -84,15 +142,105 @@ function replyKeyBoard(param) {
 }
 
 //Функція запуску загального меню
+var currentCity = "";
 function grandMenu() {
+    
     bot.on("message", (msg)=>{ 
+        let moders = db.get("moderators")
+        .value()
         //Адмін меню
         if (msg.chat.id == config.admin) {
+            citiesMassive().forEach((cur,i)=>{
+                if (cur == msg.text) {
+                    currentCity = cur;
+                    let regionkeys2 = [];
+                    let base = db.get("cities").find({name: cur}).value()
+                    base.regions.forEach((current, index)=>{
+                        let temp2 = [{"text": current.name}]
+                        regionkeys2.push(temp)
+                    })
+                    regionkeys2.push([{"text": "Додати район"}])
+                    regionkeys2.push([{"text": "/admin"}])
+                    bot.sendMessage(msg.chat.id, `Райони міста "${cur}"`, {reply_markup: {
+                        "keyboard": regionkeys2, 
+                        "resize_keyboard": true
+                    }})
+                }
+            })
             switch (msg.text) {
+                case "Додати район": 
+                    console.log(db.get("cities").find({name: currentCity}).value().regions)
+                    bot.sendMessage(msg.chat.id, "Введіть назву нового району")
+                    bot.on("text", reg=>{
+                        let regName = reg.text;
+                        let obj = {
+                            name: regName
+                        }
+                        if (reg.text != "Додати район") {
+                            let regs = db.get("cities").find({name: currentCity}).value().regions;
+                            regs.push(regName);
+                            db.get("cities")
+                            .find({name: currentCity})
+                            .assign({name: currentCity, regions: regs}) 
+                            .value();
 
-                case 'Товар':
-                    bot.sendMessage(msg.chat.id, "Список товарів", replyKeyBoard(menu.items))
+                            db.get("cities").find({name: currentCity}).value().regions
+                            .push(obj)
+                            .write();
+                            bot.sendMessage(msg.chat.id, "Район успішно додано");
+                            bot.removeListener("text")
+                        }
+                    })
                     break;
+                case 'Товар':
+                    bot.sendMessage(msg.chat.id, "Товари", replyKeyBoard(menu.items))
+                    break;
+
+                case 'Точки':
+                    bot.sendMessage(msg.chat.id, "Точки", replyKeyBoard(menu.points))
+                    break;
+
+                case 'Міста':
+                    bot.sendMessage(msg.chat.id, "Міста:", replyKeyBoard(menu.cities))
+                    let base = db.get("cities")
+                    .value()
+                    base.forEach((cur, i)=>{
+                        bot.sendMessage(msg.chat.id, cur.name, inlineKeyBoard("city", cur.name))
+                        //console.log(cur.name)
+                    })
+                    break;
+
+                case 'Райони':
+                    let regionkeys = [];
+                    citiesMassive().forEach((cur,i)=>{
+                        let temp = [{"text": cur}]
+                        regionkeys.push(temp)
+                    })
+                    regionkeys.push([{"text": "/admin"}])
+                    bot.sendMessage(msg.chat.id, "Меню районів", {reply_markup: {
+                        "keyboard": regionkeys, 
+                        "resize_keyboard": true
+                    }})
+                    break;
+                
+                case 'Додати місто':
+                    bot.sendMessage(msg.chat.id, "Введіть назву міста.")
+                    bot.on("text", city=>{
+                        let cityName = city.text;
+                        let obj = {
+                            name: cityName,
+                            regions: []
+                        }
+                        if (city.text != "Додати місто") {
+                            db.get("cities")
+                            .push(obj)
+                            .write();
+                            bot.sendMessage(msg.chat.id, "Місто успішно додано");
+                            bot.removeListener("text")
+                        }
+                    })
+                    break;
+                    
 
                 case 'Список товарів':
                     let massive = db.getState('items')
@@ -142,9 +290,41 @@ function grandMenu() {
                 case '/admin':
                         bot.sendMessage(msg.chat.id, 'Вітаю у адмін панелі!', replyKeyBoard(menu.admin));
                     break;
+                case 'До головної':
+                    bot.removeListener("text");
+                    bot.sendMessage(msg.chat.id, 'Адмін панель', replyKeyBoard(menu.admin));
+                    break;
             }
         }
+
+        //Модер меню
          
+        if (moders.some((cur, i)=>{
+            return cur == msg.chat.id
+        })) {
+            switch(msg.text) {
+                case 'Додати товар в район':
+                    let add = citiesMassive()
+                    let regionkeys2 = [];
+                    add.forEach((current, index)=>{
+                        console.log(current)
+                        let temp2 = [{"text": current}]
+                        regionkeys2.push(temp2)
+                    })
+                    regionkeys2.push([{"text": "/moder"}])
+                    console.log(regionkeys2)
+                    bot.sendMessage(msg.chat.id, `Міста`, {reply_markup: {
+                        "keyboard": regionkeys2, 
+                        "resize_keyboard": true
+                    }})
+                    //bot.sendMessage(msg.chat.id, "Додати товар")
+                    break;
+                case '/moder':
+                    bot.sendMessage(msg.chat.id, "Меню модератора", replyKeyBoard(menu.moder))
+                    break;
+            }
+        }
+
         //Юзер меню
         switch (msg.text) {
 
